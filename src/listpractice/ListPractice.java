a2baefb90fbd99de973bb9d20f86fd695c60525a
class <class@CST_N418_01.cst.citytech.cuny.edu>
2015-02-23 13:18:09 -0500

    LIstPractice

---
+++ src/listpractice/ListPractice.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package listpractice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author class
+ */
+public class ListPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     List<String>StringList = new ArrayList<String>();
+     
+     StringList.add("Noe");
+    StringList .add("Larry");
+    StringList .add("Curly");
+     
+    System.out.println(Strings.size());
+     System.out.println(Strings.get(0));
+      System.out.println(Strings.get(1));
+    }
+}
